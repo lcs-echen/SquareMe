b6b92d1935bcf1d1d0599b86dd1b4b30f644850d
Evelyn(Yuyang) Chen <yychen@lcs.on.ca>
2023-02-02 14:36:44 -0500

    finished

--- SquareMe/New Group/CalculationView.swift
+++ SquareMe/New Group/CalculationView.swift
@@ -12,6 +12,7 @@ struct CalculationView: View {
     @State var inputGiven: String = ""
     // MARK: Computed Properties
     
+    // Step 2
     // Convert the input given into an optional Double
     var inputGivenAsOptionalDouble: Double? {
         guard let inputDouble = Double(inputGiven) else {
@@ -19,16 +20,31 @@ struct CalculationView: View {
         }
         return inputDouble
     }
+    // Step 3
+    // Take the input and format it for output
+    var formattedOutputValue: String {
+        guard let numberToSquare = inputGivenAsOptionalDouble else {
+            return "Please enter a numeric value such as: 5.0"
+        }
+        let squaredNumber = numberToSquare * numberToSquare
+        
+        // Step 4
+        return squaredNumber.formatted(.number.precision(.fractionLength(2)))
+    }
     var body: some View {
         VStack {
-            VStack {
+            VStack (alignment: .center){
+                // Step 1
                 TextField("5.5", text: $inputGiven)
                     .font((.largeTitle))
-                    .padding()
+                    .padding(.leading, 30)
+                    .padding(.top, 30)
+                Text(formattedOutputValue)
+                    .font(.largeTitle)
             }
             .navigationTitle("Square Me")
             
-            
+            Spacer()
         }
     }
    
