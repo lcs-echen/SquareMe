c8cce162976f9754ff63aa558dbd0a0bac282f8c
Evelyn(Yuyang) Chen <yychen@lcs.on.ca>
2023-02-02 14:11:08 -0500

    Added a title

--- SquareMe/New Group/CalculationView.swift
+++ SquareMe/New Group/CalculationView.swift
@@ -8,9 +8,21 @@
 import SwiftUI
 
 struct CalculationView: View {
+    // MARK: Stored Properties
+    
+    // MARK: Computed Properties
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("2")
+        }
+        .navigationTitle("Square Me")
+        
+        VStack {
+            Text("2")
+        }
+        .navigationTitle("Square Me")
     }
+   
 }
 
 struct CalculationView_Previews: PreviewProvider {
